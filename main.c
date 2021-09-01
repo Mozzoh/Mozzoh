046dde9a0b41c21053284f53d113b6fccd0abd48
Paul Sylvester <27riverpaul@gmail.com>
2021-09-01 21:54:51 +0100

    add submodule glfw

---
+++ main.c
@@ -0,0 +1,12 @@
+  #include <stdio.h>
+  #include <adder.h>
+  
+  int main(int argc, char const *argv[])
+  {
+    /* code */
+    printf("%s\n","\nHello Library World!\n" );
+    //printf("\n");
+    printf("Testing add: %5.2f\n", add(1.3f, 77.6f));
+
+    return 0;
+  }

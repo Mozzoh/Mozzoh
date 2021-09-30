5b2a7a62237926b3a16557539216c64386c18bec
PaulSylvester <27riverpaul@gmail.com>
2021-09-30 16:57:01 +0100

    changed .h to .hpp and .c to .cpp

--- Adder/adder.cpp
+++ Adder/adder.cpp
@@ -1,4 +1,4 @@
-#include "adder.h"
+#include "adder.hpp"
 
 float add(float a, float b){
   return (a + b);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <adder.h>
+#include <adder.hpp>
 
 #include <glad/glad.h>
 //#ifdef __APPLE__

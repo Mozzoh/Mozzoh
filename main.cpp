a8e4ba5b6f6392bc740855dcf33d8e03b583c52d
PaulSylvester <27riverpaul@gmail.com>
2021-09-30 22:41:27 +0100

    added auto version numbering

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+
+#include <iostream>
+
 #include <adder.hpp>
 
 #include <glad/glad.h>
@@ -9,80 +12,81 @@
 //#include <GL/glut.h>
 //#endif
 #include <GLFW/glfw3.h>
+
+#include <HLIBConfig.h>
 //#include <ft2build.h>
 //#include FT_FREETYPE_H
 
-
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, 1);
 }
 
 int main(int argc, char const *argv[])
 {
-  /* code */
-  printf("%s\n","\nHello Library World!\n" );
-  //printf("\n");
-  printf("Testing add: %5.2f\n", add(1.3f, 77.6f));
+    /* code */
+    //printf("%s\n","\nHello Library World!\n" );
+    //printf("\n");
+    //printf("Testing add: %5.2f\n", add(1.3f, 77.6f));
 
-  GLFWwindow* window;
-  int width, height;
-
-  //unsigned int texture;
-  //glGenTextures(1, &texture);
+    std::cout << argv[0] << " Version " << HLIB_VERSION_MAJOR << "." << HLIB_VERSION_MINOR << '\n';
 
 
-  if( !glfwInit() )
-  {
-      fprintf( stderr, "Failed to initialize GLFW\n" );
-      exit( EXIT_FAILURE );
-  }
+    GLFWwindow *window;
+    int width, height;
 
+    //unsigned int texture;
+    //glGenTextures(1, &texture);
 
-  window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
-  if (!window)
-  {
-      fprintf( stderr, "Failed to open GLFW window\n" );
-      glfwTerminate();
-      exit( EXIT_FAILURE );
-  }
-  glfwMakeContextCurrent(window);
-  glfwSetWindowOpacity(window, 0.2f);
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
 
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetWindowOpacity(window, 0.2f);
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-  {
-      printf("%s\n", "Failed to initialize GLAD");
-      return -1;
-  }
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("%s\n", "Failed to initialize GLAD");
+        return -1;
+    }
 
-  //FT_Library ft;
+    //FT_Library ft;
 
-  //if(FT_Init_FreeType(&ft)) {
+    //if(FT_Init_FreeType(&ft)) {
     //fprintf(stderr, "Could not init freetype library\n");
     //return 1;
-  //}
+    //}
 
-  // Main loop
-  while( !glfwWindowShouldClose(window) )
-  {
-      // Swap buffers
-      glfwSwapBuffers(window);
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
 
-      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-      processInput(window);
-      //glfwPollEvents();
-      glfwWaitEvents();
-  }
+        processInput(window);
+        //glfwPollEvents();
+        glfwWaitEvents();
+    }
 
-  // Terminate GLFW
-  glfwTerminate();
-  printf("%s\n", "Terminated . . ." );
-  // Exit program
-  exit( EXIT_SUCCESS );
+    // Terminate GLFW
+    glfwTerminate();
+    printf("%s\n", "Terminated . . .");
+    // Exit program
+    exit(EXIT_SUCCESS);
 
-  return 0;
+    return 0;
 }

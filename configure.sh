c3ba5e6f56f4fb4e3cea17e79d3af6f644cbf5be
PaulSylvester <27riverpaul@gmail.com>
2021-09-30 20:56:32 +0100

    testing local and remote commit

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/sh
 
+#
 #use -D with option form external/glfw/CMakeLists.txt
 #options can be turned ON or OFF using the -D
 #cmake -D CMAKE_C_COMPILER=/usr/bin/gcc -D CMAKE_CXX_COMPILER=/usr/bin/g++ -DGLFW_BUILD_DOCS=OFF -S . -B out/build/
+
 cmake -DGLFW_BUILD_DOCS=OFF -S . -B out/build/
+   
